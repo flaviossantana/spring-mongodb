885be85cdc6130fef11a714fe21b66e8973b6cb7
flavio.santana <9090flavio@gmail.com>
2023-11-03 10:16:46 -0300

    feat: Criação do projeto web

---
+++ src/test/java/org/spring/mongo/SpringMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.spring.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
